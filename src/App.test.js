cd4cad86dadf6fd766ebaba12efef4eba56e53ae
Dave Lively <davelively14@gmail.com>
2018-01-08 21:29:44 -0500

    Test App via enzyme shallow rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('App renders without crashing', () => {
+  // Shallow renders the current node and returns a shallow wrapper around it.
+  const component = shallow(<App />);
+  expect(component.exists()).toEqual(true);
 });
