3f41c4328a2741464eaca25537f637eb1d6e1327
Dave Lively <davelively14@gmail.com>
2018-01-08 21:44:17 -0500

    Added notes for installing test suite

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,6 @@
+// For unit test w/ enzyme: npm install --save-dev enzyme react-test-renderer enzyme-adapter-react-16
+// For e2e: npm install --save-dev selenium-standalone webdriverio chai
+
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
